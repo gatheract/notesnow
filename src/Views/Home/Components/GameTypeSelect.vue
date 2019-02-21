59b6af6ea617cf3ad0954cd1d32fb744cb53f44e
Ariel <1263356+Enchufadoo@users.noreply.github.com>
2019-02-21 07:47:04 -0300

    stuff

---
+++ src/Views/Home/Components/GameTypeSelect.vue
@@ -0,0 +1,42 @@
+<template>
+  <div>
+    <div id="levelRadio">  
+      <el-radio-group v-on:input="changeGameType" v-bind:value="gameType" size="small">
+        <el-radio class="gameTypeOption" :label="GameType.game" border>{{$t("mainScreen.game")}}</el-radio>
+        <el-radio class="gameTypeOption" :label="GameType.practice" border >{{$t("mainScreen.practice")}}</el-radio>
+      </el-radio-group>
+    </div>
+  </div>
+</template>
+<script lang="ts">
+import { Component, Prop, Vue } from 'vue-property-decorator'
+import { GameType } from '@/Store/Modules/Settings/Types'
+import { namespace } from 'vuex-class'
+import { GAME_TYPE } from '@/Store/Modules/Settings/Getters'
+import { SET_GAME_TYPE } from '@/Store/Modules/Settings/Actions'
+const settingsModule = namespace('Settings')
+@Component
+export default class GameTypeSelect extends Vue {
+  @settingsModule.Getter(GAME_TYPE)
+  private gameType: GameType
+  
+  @settingsModule.Action(SET_GAME_TYPE)
+  private setGameType: any
+  
+  private data() {
+    return {
+      GameType
+    }
+  }
+  private changeGameType(type: GameType) {
+    this.setGameType(type)    
+  }
+}
+</script>
+
+<style scoped lang="scss">
+.gameTypeOption{
+  margin: 0!important;
+}
+
+</style>
